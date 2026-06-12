99ef872461fe24f29a3dfefe80bca92efd65a3d1
xyz-1208 <2871727161@qq.com>
2026-06-12 17:43:09 +0800

    版本2：增加键盘输入功能，更新README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GitHub-Zuoye
 实验1：HelloWorld 程序
+- 版本1：输出HelloWorld
+- 版本2：支持键盘输入并输出
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main() {
-    printf("Hello World!\n");
+    char name[50];
+    printf("请输入内容：\n");
+    scanf("%s", name);
+    printf("你输入的是：%s\n", name);
     return 0;
 }
